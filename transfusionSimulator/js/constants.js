c630a459642ff4e605c2bf9399faae0a91e2ee51
Nick Reynolds <ncmreynolds@googlemail.com>
2026-04-28 12:03:57 +0100

    Fix duplicate constant

--- transfusionSimulator/js/constants.js
+++ transfusionSimulator/js/constants.js
@@ -37,28 +37,28 @@ const bleScenarioAvailableResponse = bleScenarioAvailableRequest | 128;
 const bleScenarioAvailableBloodTypesRequest = 19;		//Ask for available blood types
 const bleScenarioAvailableBloodTypesResponse = bleScenarioAvailableBloodTypesRequest | 128;
 
-const bleScenarioBloodTypeUpdateRequest = 20;		//Ask for patient blood type
-const bleScenarioBloodTypeUpdateResponse = bleScenarioBloodTypeUpdateRequest | 128;
+const bleScenarioBloodTypeRequest = 20;		//Ask for patient blood type
+const bleScenarioBloodTypeResponse = bleScenarioBloodTypeRequest | 128;
 
-const bleScenarioNameLengthUpdateRequest = 21;		//Ask for name length
+const bleScenarioNameLengthUpdateRequest = 21;		//Set name length
 const bleScenarioNameLengthUpdateResponse = bleScenarioNameLengthUpdateRequest | 128;
 
-const bleScenarioNameUpdateRequest = 22;		//Ask for name data
+const bleScenarioNameUpdateRequest = 22;		//Set name data
 const bleScenarioNameUpdateResponse = bleScenarioNameUpdateRequest | 128;
 
-const bleScenarioNarrativeLengthUpdateRequest = 23;		//Ask for narrative length
+const bleScenarioNarrativeLengthUpdateRequest = 23;		//Set narrative length
 const bleScenarioNarrativeLengthUpdateResponse = bleScenarioNarrativeLengthUpdateRequest | 128;
 
-const bleScenarioNarrativeUpdateRequest = 24;		//Ask for narrative data
+const bleScenarioNarrativeUpdateRequest = 24;		//Set narrative data
 const bleScenarioNarrativeUpdateResponse = bleScenarioNarrativeUpdateRequest | 128;
 
-const bleScenarioAvailableUpdateRequest = 25;		//Ask for scenario available data
+const bleScenarioAvailableUpdateRequest = 25;		//Set scenario available data
 const bleScenarioAvailableUpdateResponse = bleScenarioAvailableUpdateRequest | 128;
 
-const bleScenarioAvailableBloodTypesUpdateRequest = 26;		//Ask for available blood types
+const bleScenarioAvailableBloodTypesUpdateRequest = 26;		//Set available blood types
 const bleScenarioAvailableBloodTypesUpdateResponse = bleScenarioAvailableBloodTypesUpdateRequest | 128;
 
-const bleScenarioBloodTypeUpdateRequest = 27;		//Ask for patient blood type
+const bleScenarioBloodTypeUpdateRequest = 27;		//Set patient blood type
 const bleScenarioBloodTypeUpdateResponse = bleScenarioBloodTypeUpdateRequest | 128;
 
 const bleDummyRequest = 127;		//Dummy value that does nothing and should never be sent
