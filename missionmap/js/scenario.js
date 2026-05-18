40ac42e9ce56ce6b69bb584cae25b70c7ac5a5b0
Nick Reynolds <ncmreynolds@googlemail.com>
2026-05-18 18:26:23 +0100

    Fix parameter typo

--- missionmap/js/scenario.js
+++ missionmap/js/scenario.js
@@ -2,5 +2,5 @@ const documentElement = document.getElementById('document');
 
 function showScenario(scenarioId)
 {
-	documentElement.innerHTML = `<iframe src="docs/${id}/" title="Mission documents" width="100%" height="800" frameborder="0" style="border:1px solid black;" ></iframe>`;
+	documentElement.innerHTML = `<iframe src="docs/${scenarioId}/" title="Mission documents" width="100%" height="800" frameborder="0" style="border:1px solid black;" ></iframe>`;
 }
